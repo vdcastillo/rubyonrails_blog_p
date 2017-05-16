03e49e47712d241b2a7730df9f3ce38117bcd4bf
Victor Castillo <vicdcastillo@gmail.com>
2017-05-16 11:21:58 -0500

    blog tutorial fitst part

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  mount Ckeditor::Engine => '/ckeditor'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "posts#index"
+
+  resources :posts
+end
